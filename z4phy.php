953e07fc04a2ce17b31b64e8fb92eb71643e65ea
Z4PHY <mahardidono@gmail.com>
2024-02-09 19:30:01 +0700

    Z4PHY

---
+++ z4phy.php
@@ -0,0 +1,13 @@
+
+<html> 
+<head> 
+<title>!.:Sweet Touch By Z4PHY:.!</title> <link href="https://fonts.googleapis.com/css?family=VT323&display=swap" rel="stylesheet">
+ <meta name="description" content="Don't panic, this is just a sweet touch from z4phy phantomhive"> <link rel="icon" type="image/png" href="https://raw.githubusercontent.com/z4phyr/z4phy-c0de/main/assets/ciel.png" </iframe> </head> <body bgcolor="black"> 	<table height="100%" width="100%"> 	
+<td align="center"> <img src="https://raw.githubusercontent.com/z4phyr/z4phy-c0de/main/assets/ciel.png" style="width: 400px;"> <br><br> <font face="VT323" color="white" size="6">Sweet Touch By <font color="red">Z4PHY PHANTOMHIVE <br><br> <font face="VT323" color="white" size="5">$ .:Please Patch Your Systems:. $<font color="red"><a href="/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail=""></a></font> <br><br> <font color="lime" size="5" face="VT323">
+ <font color="white">
+ <br> <font color="red"> </font>
+    <audio controls autoplay hidden>
+    <source src="https://raw.githubusercontent.com/z4phyr/z4phy-c0de/main/assets/get-my-way.mp3" type="audio/mpeg">
+    </audio>
+<style> .memeck { position: absolute; margin: auto; height: 50%; top: 0; bottom: 0; left: 0; right: 0; } </style> <body onclick="playAudio();" class=""><audio id="sec" __idm_id__="823202817"> 
+ <source src="https://i.top4top.io/m_1956qwpds2.mp3" type="audio/mpeg"> 		</audio> <div class="asu"> <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script> 	var x = document.getElementById("sec"); 	function playAudio() { 	x.play(); 	} 	 	function myFunction() { 		 var element = document.body; element.classList.toggle("dark-mode"); } function changeImage() { if (document.getElementById("imgClickAndChange").src == "body") { document.getElementById("imgClickAndChange").src = "body"; } else { document.getElementById("imgClickAndChange").src = "body"; } } </script> <center><br><br><br><p></p><blink>
